56711942cb82ccc89aa0715ecf0829af9736e5ee
Daniel Yang <yangsunwoo@gmail.com>
2013-01-18 13:03:35 -0500

    bad collission detection

--- models/ball.js
+++ models/ball.js
@@ -1,7 +1,7 @@
 function Ball(x, y) {
   this.x = x;
   this.y = y;
-  this.velocity = new vec2(Math.cos(2) * 3, Math.sin(2) * 3);
+  this.velocity = new vec2(Math.cos(45) * 3, Math.sin(45) * 3);
   this.radius = 12;
 }
 
@@ -14,4 +14,26 @@ Ball.prototype.draw = function() {
 Ball.prototype.update = function() {
   this.x += this.velocity.x;
   this.y += this.velocity.y;
+}
+
+function checkCollission() {
+  if (ball.y > 600) {
+    ball.newYVelocity();
+  } else if ( ball.y < 5) {
+    ball.newYVelocity();
+  } else if ( ball.x > WIDTH) {
+    ball.newXVelocity();
+  } else if (ball.x < 0) {
+    ball.newXVelocity();
+  }
+}
+
+Ball.prototype.newYVelocity = function() {
+  this.velocity.x *= 1;
+  this.velocity.y *= -1;
+}
+
+Ball.prototype.newXVelocity = function() {
+  this.velocity.x *= -1;
+  this.velocity.x *= 1;
 }
