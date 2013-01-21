fa00bb1b164cd52c9d0cdc2975219a58d1f80c7b
Daniel Yang <yangsunwoo@gmail.com>
2013-01-21 16:53:48 -0500

    underscore convention

--- entities/resize.js
+++ entities/resize.js
@@ -1,23 +1,23 @@
 function resize() {
   //resize window to fit screen
-  var gameArea = document.getElementById('gameArea');
-  var widthToHeight = 4 / 3;
+  var _gameArea = document.getElementById('gameArea');
+  var _widthToHeight = 4 / 3;
   newWidth = window.innerWidth;
   newHeight = window.innerHeight;
-  var newWidthToHeight = newWidth / newHeight;
+  var _newWidthToHeight = newWidth / newHeight;
 
-  if (newWidthToHeight > widthToHeight) {
-      newWidth = newHeight * widthToHeight;
-      gameArea.style.height = newHeight + 'px';
-      gameArea.style.width = newWidth + 'px';
+  if (_newWidthToHeight > _widthToHeight) {
+      newWidth = newHeight * _widthToHeight;
+      _gameArea.style.height = newHeight + 'px';
+      _gameArea.style.width = newWidth + 'px';
   } else {
-      newHeight = newWidth / widthToHeight;
-      gameArea.style.width = newWidth + 'px';
-      gameArea.style.height = newHeight + 'px';
+      newHeight = newWidth / _widthToHeight;
+      _gameArea.style.width = newWidth + 'px';
+      _gameArea.style.height = newHeight + 'px';
   }
 
-  gameArea.style.marginTop = (-newHeight / 2) + 'px';
-  gameArea.style.marginLeft = (-newWidth / 2) + 'px';
+  _gameArea.style.marginTop = (-newHeight / 2) + 'px';
+  _gameArea.style.marginLeft = (-newWidth / 2) + 'px';
 
   canvas = document.getElementById('c');
   ctx = canvas.getContext('2d');
